66aec0c93013a2bdb113af4b0e47a07eeba2e610
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2023-01-12 00:58:26 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 require 'vendor/autoload.php';
 
 $browserFactory = new HeadlessChromium\BrowserFactory('google-chrome');
-$browser = $browserFactory->createBrowser();
+$browser = $browserFactory->createBrowser([
+  'debugLogger' => 'php://stdout',
+]);
 $page = $browser->createPage();
 $page->navigate($argv[1])->waitForNavigation();
 echo strlen($page->getHtml());
